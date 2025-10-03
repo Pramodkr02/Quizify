d245a74c9a7a89d0c6d583289c11c2a04ce06c97
Pramodkr02 <pramodkr02.info@gmail.com>
2025-10-03 20:39:07 +0530

    Initial commit: setup project structure

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
