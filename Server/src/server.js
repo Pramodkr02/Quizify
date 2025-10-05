10a61ad1bc57675ce8af76eff52789b6fee7cbc9
Pramodkr02 <pramodkr02.info@gmail.com>
2025-10-06 01:34:44 +0530

    Fix env files

--- Server/src/server.js
+++ Server/src/server.js
@@ -15,6 +15,7 @@ await connectDB();
 app.use(
   cors({
     origin: process.env.CLIENT_URL, // set to frontend URL in Vercel
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
